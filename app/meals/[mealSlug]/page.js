783ad409567fc0f5ab19b3bbefd7fdc24145050f
Sam Turner <sam.turner@kokadjo.net>
2025-12-10 22:12:23 -0500

    finished troubleshooting MealsPage, created ShareMealPage, and ImagePicker

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,32 +1,43 @@
 import Image from 'next/image';
 import { getMeal } from '@/lib/meals';
 import classes from './page.module.css';
+import { notFound }  from 'next/navigation';
 
 export default async function MealDetailsPage({ params }) {
     const parm = await params;
     const meal = getMeal(parm.mealSlug);
 
+    if (!meal) {
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
     console.log('console.log parm?', parm.mealSlug);
     return (
         <>
             <header className={classes.header}>
                 <div className={classes.image}>
-
+                    <Image src={meal.image} alt={meal.title} fill />
                 </div>
                 <div className={classes.headerText}>
                     <h1>{meal.title}</h1>
                     <p className={classes.creator}>
-                        by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
                     </p>
                     <p className={classes.summary}>
-
+                        {meal.summary}
                     </p>
 
                 </div>
 
             </header>
             <main className={classes.main}>
+                <p className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}
 
+                ></p>
             </main>
 
 
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,63 @@
+import classes from './page.module.css';
+import ImagePicker from '@/app/components/meals/image-picker';
 export default function ShareMealPage() {
-    return (
-        <main>
-            <h1>Sharing Page</h1>
-            <p>Sharing the wonderful food.</p>
-        </main>
-    );
+    async function shareMeal(formData) {
+        'use server';
 
+        const meal = {
+            title: formData.get('title'),
+            image: formData.get('image'),
+            summary: formData.get('summary'),
+            instructions: formData.get('instructions'),
+            creator: formData.get('name'),
+            creator_email: formData.get('email')
+        }
+        console.log(meal);
+    }
+
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Share your <span className={classes.highlight}>favorite meal</span>
+                </h1>
+                <p>Or any other meal you feel needs sharing!</p>
+            </header>
+            <main className={classes.main}>
+                <form className={classes.form}>
+                    <div className={classes.row}>
+                        <p>
+                            <label htmlFor="name">Your name</label>
+                            <input type="text" id="name" name="name" required />
+                        </p>
+                        <p>
+                            <label htmlFor="email">Your email</label>
+                            <input type="email" id="email" name="email" required />
+                        </p>
+                    </div>
+                    <p>
+                        <label htmlFor="title">Title</label>
+                        <input type="text" id="title" name="title" required />
+                    </p>
+                    <p>
+                        <label htmlFor="summary">Short Summary</label>
+                        <input type="text" id="summary" name="summary" required />
+                    </p>
+                    <p>
+                        <label htmlFor="instructions">Instructions</label>
+                        <textarea
+                            id="instructions"
+                            name="instructions"
+                            rows="10"
+                            required
+                        ></textarea>
+                    </p>
+                    <ImagePicker label="Your Image" name="image" />
+                    <p className={classes.actions}>
+                        <button type="submit">Share Meal</button>
+                    </p>
+                </form>
+            </main>
+        </>
+    );
 }
